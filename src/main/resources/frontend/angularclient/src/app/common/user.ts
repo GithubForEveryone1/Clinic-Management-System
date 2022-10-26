6b03a016abfa8441bfd4f1b33e24e47222388c2d
eunice chin <ecyl2461@gmail.com>
2022-10-26 13:47:18 +0800

    added constructor details in user.ts

--- src/main/resources/frontend/angularclient/src/app/common/user.ts
+++ src/main/resources/frontend/angularclient/src/app/common/user.ts
@@ -1,2 +1,15 @@
 export class User {
+    constructor(private user_id: int,
+                private first_name: string,
+                private last_name: string,
+                private email: string,
+                private address: string,
+                private contact_number: int,
+                private password: string,
+                private dob: Date,
+                private gender: string,
+                private account_type: string,
+                private date_created: Date
+    )
 }
+	
