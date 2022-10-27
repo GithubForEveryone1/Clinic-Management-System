ff63494fe2b1bdb108763fca902be2885c05238e
Eunice <ecyl2461@gmail.com>
2022-10-27 11:08:29 +0800

    pulling from main

---
+++ target/classes/frontend/angularclient/src/app/common/user.ts
@@ -0,0 +1,17 @@
+export class User {
+    constructor(public user_id: any,
+                public first_name: string,
+                public last_name: string,
+                public email: string,
+                public address: string,
+                public contact_number: any,
+                public password: string,
+                public dob: Date,
+                public gender: string,
+                public account_type: string,
+                public date_created: Date
+    ) {
+
+    }
+}
+	
