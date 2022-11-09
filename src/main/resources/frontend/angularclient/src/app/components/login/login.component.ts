386d2df60179e35763a94b28695b3e92c5fe8a61
Remus <remusthings@gmail.com>
2022-11-09 17:29:13 +0800

    Add routing logic to upon successful login, add reroute for illegal access to user profiles when user does not have the right role, center error message

--- src/main/resources/frontend/angularclient/src/app/components/login/login.component.ts
+++ src/main/resources/frontend/angularclient/src/app/components/login/login.component.ts
@@ -49,9 +49,22 @@ export class LoginComponent implements OnInit {
     this.badLoginAttempted = false;
     this.authenticationService.authenticateUser(this.user).subscribe(
       data => {
-          this.router.navigate(['patient']);
-          console.log(this.user)
           sessionStorage.setItem("loggedInUser", JSON.stringify(data));
+          console.log(this.user)
+          switch (data.account_type) {
+            case "patient":
+              this.router.navigate(['patient']);
+              break
+            case "doctor":
+              this.router.navigate(['doctor']);
+              break
+            case "nurse":
+              this.router.navigate(['nurse']);
+              break
+              
+            default:
+              console.log("Something went wrong");
+            }
       },
       error => this.handleErrorResponse(error)
     )
--- src/main/resources/frontend/angularclient/src/app/components/nurse/nurse.component.ts
+++ src/main/resources/frontend/angularclient/src/app/components/nurse/nurse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nurse',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nurse.component.css']
 })
 export class NurseComponent implements OnInit {
+  loggedInUserStr: string | null = sessionStorage.getItem("loggedInUser");
 
-  constructor() { }
+  //parse JSON back to User object
+  loggedInUser = this.loggedInUserStr ? JSON.parse(this.loggedInUserStr) : null;
+  accountType = this.loggedInUser.account_type;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if (this.accountType != 'nurse') {
+      this.router.navigate(['error']);
+    }
   }
 
 }
