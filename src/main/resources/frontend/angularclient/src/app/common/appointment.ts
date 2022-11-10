9407456b113d2d9262d47e5a37c074c0979bf983
Alfreddk <102338382+Alfreddk@users.noreply.github.com>
2022-11-10 15:50:11 +0800

    revise appointment entity on backend and appointment class on frontend

--- src/main/resources/frontend/angularclient/src/app/common/appointment.ts
+++ src/main/resources/frontend/angularclient/src/app/common/appointment.ts
@@ -1,8 +1,12 @@
+import { User } from "./user";
+
 export class Appointment {
     constructor(
         public appt_id: number,
         public patient_id: number,
+        public patient: User,
         public doctor_id: number,
+        public doctor: User,
         public date_visited: string,
         public timeslot: number,
         public diagnosis: string,
