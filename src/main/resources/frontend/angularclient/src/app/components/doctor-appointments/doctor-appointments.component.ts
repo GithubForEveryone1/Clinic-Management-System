22d8c34ad8c5b929f916c190005cdf0ef83b2bfe
eunice chin <ecyl2461@gmail.com>
2022-11-16 11:07:47 +0800

    datatable is showing

--- src/main/resources/frontend/angularclient/src/app/components/doctor-appointments/doctor-appointments.component.ts
+++ src/main/resources/frontend/angularclient/src/app/components/doctor-appointments/doctor-appointments.component.ts
@@ -2,14 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Appointment } from 'src/app/common/appointment';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppointmentService } from 'src/app/services/appointment.service';
-import { HttpErrorResponse, HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
-// class DataTablesResponse {
-//   data: any[];
-//   draw: number;
-//   recordsFiltered: number;
-//   recordsTotal: number;
-// }
+//so that jquery can work
+declare const $:any;
 
 @Component({
   selector: 'app-doctor-appointments',
@@ -33,9 +29,10 @@ export class DoctorAppointmentsComponent implements OnInit {
   //Error message from backend server
   errorMsg = "";
 
-  constructor(private router: Router, private appointmentService: AppointmentService, private route: ActivatedRoute, private http:HttpClient) { }
+  constructor(private router: Router, private appointmentService: AppointmentService, private route: ActivatedRoute) { }
 
    ngOnInit(): void {
+    $('#example').DataTable();
      this.appointmentService.getApptsByDoctorId(this.doctorId).subscribe(
        data => {
          this.appts = data;
@@ -45,33 +42,7 @@ export class DoctorAppointmentsComponent implements OnInit {
        error => this.handleErrorResponse(error),
      );
    }
-  // ngOnInit(): void {
-  //   const that = this;
 
-  //   this.dtOptions = {
-  //     pagingType: 'full_numbers',
-  //     pageLength: 2,
-  //     serverSide: true,
-  //     processing: true,
-  //     ajax: (dataTablesParameters: any, callback) => {
-  //       that.http
-  //         .post<DataTablesResponse>(
-  //           'https://xtlncifojk.eu07.qoddiapp.com/',
-  //           dataTablesParameters, {}
-  //         ).subscribe(resp => {
-  //           that.persons = resp.data;
-
-  //           callback({
-  //             recordsTotal: resp.recordsTotal,
-  //             recordsFiltered: resp.recordsFiltered,
-  //             data: []
-  //           });
-  //         });
-  //     },
-  //     columns: [{ data: 'id' }, { data: 'firstName' }, { data: 'lastName' }]
-  //   };
-  // }
-  
   handleErrorResponse(error:HttpErrorResponse) {
     this.errorMsg = error.error.message;
   }
