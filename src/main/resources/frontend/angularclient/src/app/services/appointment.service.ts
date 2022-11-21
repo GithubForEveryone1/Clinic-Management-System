b5ac9c576f82920c9a03b0289b15512fc8d5331d
eunice chin <ecyl2461@gmail.com>
2022-11-21 12:15:58 +0800

    amended service in angular to match with new edit controller

--- src/main/resources/frontend/angularclient/src/app/services/appointment.service.ts
+++ src/main/resources/frontend/angularclient/src/app/services/appointment.service.ts
@@ -33,8 +33,8 @@ export class AppointmentService {
     return this.httpClient.get<Appointment[]>(`${this.baseUrl}/docAppt/` + userId);
   }
 
-  editAppt(appointment: any): Observable<Appointment> {
-		return this.httpClient.put<Appointment>(`${this.baseUrl}/appt/edit`, appointment);
+  editApptDiagnosisAndPrescription(appointment: any): Observable<Appointment> {
+		return this.httpClient.put<Appointment>(`${this.baseUrl}/appt/edit-diagnosis-and-prescription`, appointment);
 	}
 
 }
